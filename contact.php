2018b8da517287fc968582a485640e3e218d7f18
yicheng zhao <zhaoyic@gmail.com>
2014-08-14 16:19:40 +0800

    三个底部导航页面正核

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+// 导航 当前页面控制
+$current_page = 'contact';
+$page_level = explode('-', $current_page);
+
+
+include 'view/contact_page.php';
--- view/footer.php
+++ view/footer.php
@@ -2,10 +2,10 @@
     <div class="middle-wide">
         <div class="logo"><img src="asset/6.png" alt=""></div>
         <ul>
-            <li><a href="#">关于我们</a></li>
-            <li><a href="#">免责声明</a></li>
-            <li><a href="#">联系方式</a></li>
-            <li><a href="#">帮助中心</a></li>
+            <li><a href="about.php">关于我们</a></li>
+            <li><a href="disclaimer.php">免责声明</a></li>
+            <li><a href="contact.php">联系方式</a></li>
+            <li><a href="mailto:help@tobemaker.com">帮助中心</a></li>
             <br class="clear"/>
         </ul>
         <p>Copyright@2012-2014 .All Rights Reserved. Powered by 呆萌科技.</p>
