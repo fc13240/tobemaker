d7b2f074c006313dfa312cea8074ef8b4ceecf47
zhenzhou yang <1020378873@qq.com>
2014-08-18 14:29:10 +0800

    展示接口

---
+++ api/project_list.php
@@ -0,0 +1,16 @@
+<?php
+include_once ROOT_PATH."class/class_idea.php";
+include_once ROOT_PATH."class/class_like.php";
+	$class_idea=new class_idea();
+	$num=$class_idea->get_num_of_passed();
+	$current_page=$_POST['current_page'];
+	$start=($current_page-1)*6;
+	$res=$class_idea->get_part_passed($start,6);
+	$records=array();
+	$records['data']=array();
+	$records['data']=$res;
+	$records['current_page']=$current_page;
+	$records['num_of_currentpage']=count($res);
+
+	echo json_encode($records);
+?>
