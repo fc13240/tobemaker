04ad9ae397783fbae5826ebae82d68546d0655a8
zhenzhou yang <1020378873@qq.com>
2014-08-20 09:36:25 +0800

    搜索接口和前台数据获取接口改变

--- api/user_search.php
+++ api/user_search.php
@@ -2,31 +2,26 @@
 include_once "../config.php";
 include_once ROOT_PATH."class/class_idea.php";
 $class_idea=new class_idea();
-$sum_of_all=null;
-$current_page=null;
+$num_of_all=null;
 
-$res['sum_of_all']=null;
 if(array_key_exists('q', $_GET)){
 	$key_word=$_GET['q'];
-	if(isset($sum_of_all)){
-	}
 
-	else{
 		# code...
 		$data=$class_idea->search_all_by_key_word($key_word);
-		$sum_of_all=count($data);
-	}
+		$num_of_all=count($data);
 		$start=$_GET['start'];
 		$length=$_GET['length'];
-		$data=$class_idea->search_by_key_word($key_word,$start,$length);
-		//var_dump($data);
+		$data=array_slice($data,$start,$length);
 
-		$result['sum_of_all']=$sum_of_all;
+		$result['num_of_all']=$num_of_all;
 		$result['num_of_currentpage']=count($data);
 		$result['data']=$data;
-
 		echo json_encode($result);
+}
 
+else{
+	echo "no key_word";
 }
 
 ?>
