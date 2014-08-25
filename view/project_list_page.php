3bd6822fc857a75f1a95a09821e914b738db6302
yicheng zhao <zhaoyic@gmail.com>
2014-08-25 12:40:28 +0800

    项目页项目列表ajax抓取数据框架完成

--- view/project_list_page.php
+++ view/project_list_page.php
@@ -14,109 +14,111 @@
         <div class="slogan">
             大家的想法都在这里，喜欢的话不妨点赞支持一下吧！
         </div>
-        <div class="content list">
-            <dl>
-                <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
-                <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
-                <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
-                <dd>
-                    <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
-                    <a href="#" class="author">糯米团子</a>
-                    <span>发布</span>
-                </dd>
-                <dd>
-                    <div class="button"><a href="#">分享</a></div>
-                    <div class="button"><a href="#">评论</a><span>23</span></div>
-                    <div class="button"><a href="#">超喜欢</a><span>23</span></div>
-                </dd>
+        <div id="idea-list-block" class="content list">
+            <div id="idea-list-content">
+                <dl>
+                    <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
+                    <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
+                    <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
+                    <dd>
+                        <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
+                        <a href="#" class="author">糯米团子</a>
+                        <span>发布</span>
+                    </dd>
+                    <dd>
+                        <div class="button"><a href="#">分享</a></div>
+                        <div class="button"><a href="#">评论</a><span>23</span></div>
+                        <div class="button"><a href="#">超喜欢</a><span>23</span></div>
+                    </dd>
 
-            </dl>
-            <dl>
-                <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
-                <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
-                <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
-                <dd>
-                    <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
-                    <a href="#" class="author">糯米团子</a>
-                    <span>发布</span>
-                </dd>
-                <dd>
-                    <div class="button"><a href="#">分享</a></div>
-                    <div class="button"><a href="#">评论</a><span>23</span></div>
-                    <div class="button"><a href="#">超喜欢</a><span>23</span></div>
-                </dd>
+                </dl>
+                <dl>
+                    <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
+                    <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
+                    <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
+                    <dd>
+                        <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
+                        <a href="#" class="author">糯米团子</a>
+                        <span>发布</span>
+                    </dd>
+                    <dd>
+                        <div class="button"><a href="#">分享</a></div>
+                        <div class="button"><a href="#">评论</a><span>23</span></div>
+                        <div class="button"><a href="#">超喜欢</a><span>23</span></div>
+                    </dd>
 
-            </dl>
-            <dl>
-                <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
-                <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
-                <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
-                <dd>
-                    <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
-                    <a href="#" class="author">糯米团子</a>
-                    <span>发布</span>
-                </dd>
-                <dd>
-                    <div class="button"><a href="#">分享</a></div>
-                    <div class="button"><a href="#">评论</a><span>23</span></div>
-                    <div class="button"><a href="#">超喜欢</a><span>23</span></div>
-                </dd>
+                </dl>
+                <dl>
+                    <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
+                    <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
+                    <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
+                    <dd>
+                        <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
+                        <a href="#" class="author">糯米团子</a>
+                        <span>发布</span>
+                    </dd>
+                    <dd>
+                        <div class="button"><a href="#">分享</a></div>
+                        <div class="button"><a href="#">评论</a><span>23</span></div>
+                        <div class="button"><a href="#">超喜欢</a><span>23</span></div>
+                    </dd>
 
-            </dl>
-            <dl>
-                <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
-                <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
-                <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
-                <dd>
-                    <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
-                    <a href="#" class="author">糯米团子</a>
-                    <span>发布</span>
-                </dd>
-                <dd>
-                    <div class="button"><a href="#">分享</a></div>
-                    <div class="button"><a href="#">评论</a><span>23</span></div>
-                    <div class="button"><a href="#">超喜欢</a><span>23</span></div>
-                </dd>
+                </dl>
+                <dl>
+                    <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
+                    <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
+                    <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
+                    <dd>
+                        <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
+                        <a href="#" class="author">糯米团子</a>
+                        <span>发布</span>
+                    </dd>
+                    <dd>
+                        <div class="button"><a href="#">分享</a></div>
+                        <div class="button"><a href="#">评论</a><span>23</span></div>
+                        <div class="button"><a href="#">超喜欢</a><span>23</span></div>
+                    </dd>
 
-            </dl>
-            <dl>
-                <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
-                <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
-                <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
-                <dd>
-                    <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
-                    <a href="#" class="author">糯米团子</a>
-                    <span>发布</span>
-                </dd>
-                <dd>
-                    <div class="button"><a href="#">分享</a></div>
-                    <div class="button"><a href="#">评论</a><span>23</span></div>
-                    <div class="button"><a href="#">超喜欢</a><span>23</span></div>
-                </dd>
+                </dl>
+                <dl>
+                    <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
+                    <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
+                    <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
+                    <dd>
+                        <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
+                        <a href="#" class="author">糯米团子</a>
+                        <span>发布</span>
+                    </dd>
+                    <dd>
+                        <div class="button"><a href="#">分享</a></div>
+                        <div class="button"><a href="#">评论</a><span>23</span></div>
+                        <div class="button"><a href="#">超喜欢</a><span>23</span></div>
+                    </dd>
 
-            </dl>
-            <dl>
-                <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
-                <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
-                <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
-                <dd>
-                    <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
-                    <a href="#" class="author">糯米团子</a>
-                    <span>发布</span>
-                </dd>
-                <dd>
-                    <div class="button"><a href="#">分享</a></div>
-                    <div class="button"><a href="#">评论</a><span>23</span></div>
-                    <div class="button"><a href="#">超喜欢</a><span>23</span></div>
-                </dd>
+                </dl>
+                <dl>
+                    <dd><a href="item.html"><img src="asset/13.png" alt=""></a></dd>
+                    <dt>云品良木@韩版 简约风 卡通动物 大容量 铁皮盒 学生文具盒 12款</dt>
+                    <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>
+                    <dd>
+                        <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>
+                        <a href="#" class="author">糯米团子</a>
+                        <span>发布</span>
+                    </dd>
+                    <dd>
+                        <div class="button"><a href="#">分享</a></div>
+                        <div class="button"><a href="#">评论</a><span>23</span></div>
+                        <div class="button"><a href="#">超喜欢</a><span>23</span></div>
+                    </dd>
 
-            </dl>
+                </dl>
+            </div>
 
             <div class="prev">
-                <div><a href="#"><</a></div>
+                <div><a href="javascript:0"><</a></div>
             </div>
             <div class="next">
-                <div><a href="#">></a></div>
+                <div><a href="javascript:0">></a></div>
             </div>
 
             <br class="clear"/>
@@ -150,26 +152,94 @@
 <!--监听浏览器长宽改变-->
 
 <script type="text/javascript" charset="utf-8">
-	function a(){
-		$(".prev").height( $(window).height() );
-		$(".prev").css("line-height" , $(window).height()+"px");
-		$(".prev").css("left" , ($(window).width() - 1020)/2 - 70 +"px");
-		$(".next").height( $(window).height() );
-		$(".next").css("line-height" , $(window).height()+"px");
-		$(".next").css("left" , ($(window).width() - 1020)/2 + 1040 +"px");
-	}
-	window.onload=function(){  
-		window.onresize= a;
-		a();
-	}
-    $("#project-pagenum").on("click","a",function(){
-        var current_page = parseInt( $(this).text() );
-        var pageSize = 6;
+    function a(){
+            $(".prev").height( $(window).height() );
+            $(".prev").css("line-height" , $(window).height()+"px");
+            $(".prev").css("left" , ($(window).width() - 1020)/2 - 70 +"px");
+            $(".next").height( $(window).height() );
+            $(".next").css("line-height" , $(window).height()+"px");
+            $(".next").css("left" , ($(window).width() - 1020)/2 + 1040 +"px");
+    }
+    
+    var pageSize = 6;
+    var pageNow = 1;
+    var maxPageNo = 1;
+    
+    function loadIdeaPage(current_page){
+        
         var start = (current_page - 1) * pageSize;
         var length = pageSize;
-        var url = $(this).parent("div").data("url");
+        var url = $('#project-pagenum').data("url");
         $.post(url, {"start":start, "length":length}, function(data, textStatus){
             console.log(data);
+            
+            // set up content
+            var $container = $('#idea-list-content');
+            $container.html('');
+            
+            for (var i=0; data.data != undefined && i<data.data.length; i++){
+                var item = data.data[i];
+
+                $container.append('\
+            <dl>\
+                <dd><a href="project.php?idea_id='+ item.idea_id +'"><img src="asset/13.png" alt=""></a></dd>\
+                <dt>'+ item.name +'</dt>\
+                <dd><div class="bar"><div class="done" style="width: 60%"></div></div></dd>\
+                <dd>\
+                    <a href="#" class="avatar"><img src="asset/12.png" alt=""></a>\
+                    <a href="#" class="author">'+ item.user_name +'</a>\
+                    <span>发布</span>\
+                </dd>\
+                <dd>\
+                    <div class="button"><a href="#">分享</a></div>\
+                    <div class="button"><a href="project.php?idea_id='+ item.idea_id +'">评论</a><span>23</span></div>\
+                    <div class="button"><a href="#">超喜欢</a><span>23</span></div>\
+                </dd>\
+            </dl>');
+    
+            }
+            
+            // set up page nav
+            var $pageNav = $('#project-pagenum');
+            $pageNav.html('');
+            maxPageNo = Math.ceil( parseInt(data.num_of_all) / pageSize );
+            for (var i=1; i<=maxPageNo && i<=9; i++){
+                var pageNavClass = "";
+                if (i == current_page){
+                    pageNavClass = ' class="active" ';
+                }
+                $pageNav.append('<a href="#" '+ pageNavClass +' >'+ i +'</a>');
+            }
+            if (maxPageNo > 9){
+                $pageNav.append('<a>...</a>');
+            }
+        },"json");
+    }
+    
+    $("#project-pagenum").on("click","a",function(){
+        var current_page = parseInt( $(this).text() );
+        pageNow = current_page;
+        loadIdeaPage(pageNow);
+    });
+    
+    $(document).ready(function(){
+        window.onresize= a;
+        a();
+        pageNow = 1;
+        loadIdeaPage(pageNow);
+        
+        $('#idea-list-block .next').click(function(){
+            if (pageNow < maxPageNo){   
+                pageNow++;
+                loadIdeaPage(pageNow);
+            }
+        });
+        
+        $('#idea-list-block .prev').click(function(){
+            if (pageNow > 1){
+                pageNow--;
+                loadIdeaPage(pageNow);
+            }
         });
     });
 </script>
