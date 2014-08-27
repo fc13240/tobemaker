12ab9a76c15545b086435e6a36a1be73c39eabed
zhenzhou yang <1020378873@qq.com>
2014-08-27 11:37:08 +0800

    修复了前台项目列表数据显示api，project_list.php  现在数据能正常显示

--- person.php
+++ person.php
@@ -1,10 +1,29 @@
 <?php
 
 include_once "config.php";
+include_once ROOT_PATH."class/class_user.php";
 
 // 导航 当前页面控制
 $current_page = 'person';
 $page_level = explode('-', $current_page);
+$class_user=new class_user();
+//  准备数据
 
+if(array_key_exists('user_id', $_GET)){
+	$user_info=$class_user->select($_GET['user_id']);
+	if(count($user_info)==0)
+	{
+		echo "no user";
+		header("Location:".BASE_URL);
+	}
+	else{
+		$user_info=$user_info[0];
+	}
+}
+else{
+	$url="Location:".BASE_URL;
+	header($url);
+
+}
 
 include 'view/person_page.php';
