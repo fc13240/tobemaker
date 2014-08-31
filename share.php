e7669359d1c7e4608e769ab96131b8a359b98fdf
yicheng zhao <zhaoyic@gmail.com>
2014-08-31 17:09:59 +0800

    debug qiniu on servser

--- share.php
+++ share.php
@@ -36,8 +36,8 @@ if(array_key_exists('img_url',$_POST))
 	$url_array = explode("/", $pic_url);
     $key = end($url_array);
     $key1 ="upload/".$current_user['user_id']."/".$key;
-    $qiniu->move($key,$key1);
-    $pic_url=QINIU_DOWN.$key1;
+//    $qiniu->move($key,$key1);
+//    $pic_url=QINIU_DOWN.$key1;
    //
 	$arr['name']=$_POST['title'];
 	$arr['content']=$_POST['content'];
