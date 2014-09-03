b1022d618937778e83b6fa35bfb4ddb812cae99b
yicheng zhao <zhaoyic@gmail.com>
2014-09-03 17:13:41 +0800

    修复后台项目详情页面富文本框宽度异常

--- admin/idea_detail_all.php
+++ admin/idea_detail_all.php
@@ -54,47 +54,4 @@ else{
 $current_page = 'idea-idea_detail_all';
 $page_level = explode('-', $current_page);
 
-$page_level_style = '
-<link rel="stylesheet" type="text/css" href="./assets/global/plugins/select2/select2.css"/>
-<link rel="stylesheet" type="text/css" href="'.BASE_URL.'css/simditor.css"/>
-';
-
-$page_level_plugins = '
-<script type="text/javascript" src="./assets/global/plugins/select2/select2.min.js"></script>
-
-';
-
-$page_level_script = '
-<script src="./assets/global/scripts/metronic.js" type="text/javascript"></script>
-<script src="./assets/admin/layout/scripts/layout.js" type="text/javascript"></script>
-<script src="./assets/admin/layout/scripts/quick-sidebar.js" type="text/javascript"></script>
-<script src="./assets/admin/layout/scripts/demo.js" type="text/javascript"></script>
-<script src="./assets/admin/pages/scripts/form-samples.js"></script>
-<script type="text/javascript" src="'.BASE_URL.'js/simditor-all.min.js"></script>
-<script>
-jQuery(document).ready(function() {    
-   // initiate layout and plugins
-   Metronic.init(); // init metronic core components
-    Layout.init(); // init current layout
-    QuickSidebar.init(); // init quick sidebar
-    Demo.init(); // init demo features
-   FormSamples.init();
-});
-'."
-var editor = new Simditor({
-    textarea: $('#editor'),
-     toolbar:  ['title', 'bold', 'italic', 'underline', 'strikethrough', 'color', '|', 'ol', 'ul', 'blockquote', 'code', 'table',  'link', 'image', 'hr', '|', 'indent', 'outdent'],
-});
-</script>
-
-";
-
-include 'view/header.php';
-
-include 'view/leftnav.php';
-
 include 'view/idea_detail_all.php';
-
-include 'view/quick_bar.php';
-
-include 'view/footer.php';
