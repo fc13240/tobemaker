911059e5255d1cd11bf93e91024f8a3d8dfb3d26
yicheng zhao <zhaoyic@gmail.com>
2014-09-05 13:19:42 +0800

    修正部署代码

--- production_1_deploy.sh
+++ production_1_deploy.sh
@@ -1,5 +1,6 @@
 rm -rf /var/www/tobemaker/*
 cp -rf * /var/www/tobemaker/
 cp /var/www/tobemaker/config.production_1.php /var/www/tobemaker/config.php
+cp .htaccess /var/www/tobemaker/.htaccess
 chmod -R 777 /var/www/tobemaker/tmp
 chmod -R 777 /var/www/tobemaker/upload
