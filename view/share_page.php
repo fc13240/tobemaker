9e1515e201af4f2f82aae3b0011d1578a53858d9
yicheng zhao <zhaoyic@gmail.com>
2014-10-06 10:45:11 +0800

    分享页搜索图标显示错位修复

--- view/share_page.php
+++ view/share_page.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html  xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>share</title>
     <?php include "top_css.php" ?>
     <link rel="stylesheet" type="text/css" href="css/redactor.css">
     <link rel="stylesheet" type="text/css" href="css/simditor.css"/>
-    <style type="text/css">
-    	.fa.fa-search {
-    		top: -4px;
-    	}
-    </style>
 </head>
 <body>
 <div id="top">
